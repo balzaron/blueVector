a06c554694a6947238be08fd68e0eaf06339ce6b
34862933@qq.com <lf34862933>
2018-08-31 10:07:08 +0800

    1

--- uesr/urls.py
+++ uesr/urls.py
@@ -10,7 +10,7 @@ urlpatterns = [
     # url(r'^', views.index,name="home"),
     url(r'^jobs/$', views.index, name="jobs"),
 url(r'^jobList/$', views.jobList),
-url(r'^jobDetail/$', views.jobDetail),
+url(r'^jobdetail/$', views.jobDetail),
 ]
 
 # urlpatterns = format_suffix_patterns(urlpatterns)
