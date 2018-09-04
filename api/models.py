a62c37ad409134b0442addfccf17fb3b1cce827f
高山岳 <shanyue.gao@hotmail.com>
2018-09-04 18:47:59 +0800

    Merge branch 'master' of https://github.com/balzaron/blueVector
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- api/models.py
+++ api/models.py
@@ -3,7 +3,6 @@ from .enums import *
 # Create your models here.
 
 
-
 class BaseModel(models.Model):
     created_time = models.DateTimeField(auto_now_add=True)
     updated_time = models.DateTimeField(auto_now=True)
@@ -11,13 +10,10 @@ class BaseModel(models.Model):
 
     class Meta:
         ordering = ('created_time', )
+        abstract = True
 
 
-class User(models.Model):
-
-    created_time = models.DateTimeField(auto_now_add=True)
-    updated_time = models.DateTimeField(auto_now=True)
-    deleted = models.BooleanField(default=False, auto_created=True)
+class User(BaseModel):
 
     name = models.CharField(max_length=100, default='')
     phone = models.CharField(max_length=11, default='')
@@ -32,25 +28,16 @@ class User(models.Model):
         ordering = ('created_time',)
 
 
-class Institution(models.Model):
-    #, auto_created=True, db_index=True, default=1
-    created_time = models.DateTimeField(auto_now_add=True)
-    updated_time = models.DateTimeField(auto_now=True)
-    deleted = models.BooleanField(default=False, auto_created=True)
+class Institution(BaseModel):
 
     name = models.CharField(max_length=100, default='')
     describe = models.CharField(max_length=500, default='')
     recommend = models.CharField(max_length=5, default='***')
     bReward = models.BooleanField(default=False)
     reward = models.IntegerField(default=200)
-    class Meta:
-        ordering = ('created_time',)
 
 
-class Agent(models.Model):
-    created_time = models.DateTimeField(auto_now_add=True)
-    updated_time = models.DateTimeField(auto_now=True)
-    deleted = models.BooleanField(default=False, auto_created=True)
+class Agent(BaseModel):
 
     name = models.CharField(max_length=100, default='')
     college = models.CharField(max_length=100, default='', blank=True)
@@ -58,14 +45,8 @@ class Agent(models.Model):
     password = models.CharField(max_length=255)
     tags1 = models.CharField(max_length=255, blank=True)
 
-    class Meta:
-        ordering = ('created_time', )
 
-
-class Education(models.Model):
-    created_time = models.DateTimeField(auto_now_add=True)
-    updated_time = models.DateTimeField(auto_now=True)
-    deleted = models.BooleanField(default=False, auto_created=True)
+class Education(BaseModel):
 
     name = models.CharField(max_length=100, default='')
     sex = models.CharField(max_length=10, choices=sexes)
@@ -77,14 +58,8 @@ class Education(models.Model):
     professional = models.CharField(max_length=100)
     note = models.TextField()
 
-    class Meta:
-        ordering = ('created_time', )
 
-
-class Jobs(models.Model):
-    created_time = models.DateTimeField(auto_now_add=True)
-    updated_time = models.DateTimeField(auto_now=True)
-    deleted = models.BooleanField(default=False, auto_created=True)
+class Jobs(BaseModel):
 
     href = models.URLField(default="http://www.lanxiangren.net")
     img = models.URLField(default=None)
@@ -95,14 +70,8 @@ class Jobs(models.Model):
     msg2 = models.CharField(max_length=210)
     msg3 = models.CharField(max_length=210)
 
-    class Meta:
-        ordering = ('created_time',)
 
-
-class JobDetail(models.Model):
-    created_time = models.DateTimeField(auto_now_add=True)
-    updated_time = models.DateTimeField(auto_now=True)
-    deleted = models.BooleanField(default=False, auto_created=True)
+class JobDetail(BaseModel):
 
     title = models.CharField(max_length=255)
     salary = models.CharField(max_length=50)
@@ -117,14 +86,8 @@ class JobDetail(models.Model):
     address = models.TextField()
     tags = models.TextField()
 
-    class Meta:
-        ordering = ('created_time', )
-
 
 class LifeLog(models.Model):
-    created_time = models.DateTimeField(auto_now_add=True)
-    updated_time = models.DateTimeField(auto_now=True)
-    deleted = models.BooleanField(default=False, auto_created=True)
 
     phone = models.CharField(max_length=200, blank=True)
     name = models.CharField(max_length=100, blank=True)
@@ -138,18 +101,3 @@ class LifeLog(models.Model):
     tags1 = models.CharField(max_length=255, blank=True)
     tags2 = models.CharField(max_length=255, blank=True)
     tags3 = models.CharField(max_length=255, blank=True)
-
-    def toJSON(self):
-        fields = []
-        for field in self._meta.fields:
-            fields.append(field.name)
-
-        d = {}
-        for attr in fields:
-            d[attr] = getattr(self, attr)
-
-        import json
-        return json.dumps(d)
-
-    class Meta:
-        ordering = ('-created_time', )
