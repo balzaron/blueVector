120a556404248debfc10828cfb8fcb3c07fcdbd3
高山岳 <shanyue.gao@hotmail.com>
2018-09-12 15:16:37 +0800

    Merge branch 'master' of https://github.com/balzaron/blueVector
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- poster.py
+++ poster.py
@@ -2,48 +2,55 @@ import time
 
 import xlrd,requests
 
-sheet = xlrd.open_workbook('./jobdetail.xlsx').sheet_by_index(1)
-fields = sheet.row_values(0)
-row_num = sheet.nrows
-col_num = sheet.ncols
-print(row_num,col_num)
+wb = xlrd.open_workbook('./jobdetail.xlsx')
+
+
+sheet_jobs = wb.sheet_by_name('job')
+sheet_train = wb.sheet_by_name('train')
+fields = sheet_jobs.row_values(0)
+
 
 jobs_url = "http://www.lanxiangren.net/api/jobs/"
-detail_url = "http://www.lanxiangren.net/api/jobdetail/"
+jobs_detail_url = "http://www.lanxiangren.net/api/jobdetail/"
+jobs_urls = [jobs_url, jobs_detail_url]
+
+train_url = 'http://www.lanxiangren.net/train/jobs/'
+train_detail_url = 'http://www.lanxiangren.net/train/jobsdetail/'
+train_urls = [train_url, train_detail_url]
+
+types = ('overview', 'detail')
 
 
-def poster(type):
+def poster(sheet, type, urls):
+
+    row_num = sheet.nrows
+    col_num = sheet.ncols
     for i in range(1,row_num):
         dict = {}
         for j in range(col_num):
-            if type != "jobs" and j==6:
+            if type != types[0] and j==6:
                 continue
             else:
                 dict.setdefault(fields[j], sheet.cell(i,j).value)
-            if type == 'jobs':
+            if type == types[0]:
                 dict.setdefault('href', 'http://www.lanxiangren.net/user/jobdetail/?id=%d'%i)
-            if type == 'jobs' and j == 6:
-                # requests.post(jobs_url, dict)
-                # dict.setdefault('img','http://www.lanxiangren.net/logo/text2.png')
-                # print(dict)
+            if type == types[0] and j == 6:
                 break
 
-
-            # res = requests.post(jobs_url, data = dict)
-
         res = None
-        if type == 'jobs':
-            print ("jobs",dict)
-            res = requests.post(jobs_url,dict)
+        if type == types[0]:
+            print ("overview",dict)
+            res = requests.post(urls[0],dict)
             print(res.text)
             pass
         else:
             print ("detail",dict)
-            res = requests.post(detail_url, dict)
+            res = requests.post(urls[1], dict)
             print(res.text)
             pass
 
 
+
 if __name__ == '__main__':
-    poster('jobs')
-    poster('')
+    for i in types:
+        poster(sheet_train, i, train_urls)
