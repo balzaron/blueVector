47a45a886543879a408b38f4465676f866cd4777
高山岳 <shanyue.gao@hotmail.com>
2018-09-12 16:22:29 +0800

    Merge branch 'master' of https://github.com/balzaron/blueVector
    # Please enter a commit message to explain why this merge is necessary,
    # especially if it merges an updated upstream into a topic branch.
    #
    # Lines starting with '#' will be ignored, and an empty message aborts
    # the commit.

--- poster.py
+++ poster.py
@@ -5,17 +5,17 @@ import xlrd,requests
 wb = xlrd.open_workbook('./jobdetail.xlsx')
 
 
-sheet_jobs = wb.sheet_by_name('job')
+sheet_jobs = wb.sheet_by_name('Sheet2')
 sheet_train = wb.sheet_by_name('train')
 fields = sheet_jobs.row_values(0)
-
+print(fields)
 
 jobs_url = "http://www.lanxiangren.net/api/jobs/"
 jobs_detail_url = "http://www.lanxiangren.net/api/jobdetail/"
 jobs_urls = [jobs_url, jobs_detail_url]
 
 train_url = 'http://www.lanxiangren.net/train/jobs/'
-train_detail_url = 'http://www.lanxiangren.net/train/jobsdetail/'
+train_detail_url = 'http://www.lanxiangren.net/train/jobdetail/'
 train_urls = [train_url, train_detail_url]
 
 types = ('overview', 'detail')
@@ -28,13 +28,13 @@ def poster(sheet, type, urls):
     for i in range(1,row_num):
         dict = {}
         for j in range(col_num):
-            if type != types[0] and j==6:
+            if type != types[0] and j==7:
                 continue
             else:
                 dict.setdefault(fields[j], sheet.cell(i,j).value)
             if type == types[0]:
-                dict.setdefault('href', 'http://www.lanxiangren.net/user/jobdetail/?id=%d'%i)
-            if type == types[0] and j == 6:
+                dict.setdefault('href', urls[1] + '?id=%d'%i)
+            if type == types[0] and j == 7:
                 break
 
         res = None
@@ -50,7 +50,7 @@ def poster(sheet, type, urls):
             pass
 
 
-
 if __name__ == '__main__':
     for i in types:
         poster(sheet_train, i, train_urls)
+        poster(sheet_jobs, i, jobs_urls)
