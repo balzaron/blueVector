325cf13d8261cb77980807f1e5f3adb81a44c3d3
34862933@qq.com <lf34862933>
2018-09-21 15:00:57 +0800

    1

--- poster.py
+++ poster.py
@@ -8,16 +8,22 @@ wb = xlrd.open_workbook('./jobdetail.xlsx')
 sheet_jobs = wb.sheet_by_name('Sheet2')
 sheet_jobs_add = wb.sheet_by_name('jobadd')
 sheet_train = wb.sheet_by_name('train')
+sheet_edu = wb.sheet_by_name('edu')
 fields = sheet_jobs.row_values(0)
 print(fields)
 
 jobs_url = "http://www.lanxiangren.net/api/jobs/"
-jobs_detail_url = "http://www.lanxiangren.net/api/jobdetail/"
-jobs_urls = [jobs_url, jobs_detail_url]
+jobs_detail_url = "http://www.lanxiangren.net/user/jobdetail/"
+jobs_urls = [jobs_url, jobs_detail_url,"http://www.lanxiangren.net/user/jobdetail/"]
 
 train_url = 'http://www.lanxiangren.net/train/jobs/'
 train_detail_url = 'http://www.lanxiangren.net/train/jobdetail/'
-train_urls = [train_url, train_detail_url]
+train_urls = [train_url, train_detail_url,'http://www.lanxiangren.net/train/jobDetail/']
+
+edu_url = 'http://www.lanxiangren.net/edu/jobs/'
+edu_detail_url = 'http://www.lanxiangren.net/edu/jobdetail/'
+edu_urls = [edu_url, edu_detail_url,'http://www.lanxiangren.net/train/jobDetail/']
+
 
 types = ('overview', 'detail')
 
@@ -34,13 +40,13 @@ def poster(sheet, type, urls):
             else:
                 dict.setdefault(fields[j], sheet.cell(i,j).value)
             if type == types[0]:
-                dict.setdefault('href', urls[1] + '?id=%d'%(i+18))
+                dict.setdefault('href', urls[2] + '?id=%d'%(i))
             if type == types[0] and j == 7:
                 break
 
         res = None
         if type == types[0]:
-            print ("overview",dict)
+            # print ("overview",dict)
             # res = requests.post(urls[0],dict)
             # print(res.text)
             pass
@@ -62,4 +68,5 @@ if __name__ == '__main__':
     # mysql()
     for i in types:
         # poster(sheet_train, i, train_urls)
-        poster(sheet_jobs_add, i, jobs_urls)
+        # poster(sheet_jobs, i, jobs_urls)
+        poster(sheet_edu, i, edu_urls)
